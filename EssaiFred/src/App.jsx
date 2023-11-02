9cafbebda868b9a708bb40681475beb8da2a13ef
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-02 11:59:22 +0100

    Create App.jsx

---
+++ EssaiFred/src/App.jsx
@@ -0,0 +1,14 @@
+import MyTitle from "./assets/components/MyTitle";
+import Fred from "./assets/components/Fred";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <MyTitle />
+      <Fred />
+    </div>
+  );
+}
+
+export default App;
